5947e156f37573b6b43fb103d8e2cbaede69f039
Dusan Cojic <dusancojic@gmail.com>
2024-02-14 15:22:38 +0100

    feat: total expense card

--- lib/screens/expense_screen.dart
+++ lib/screens/expense_screen.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:pocket_pal/widgets/expense_card.dart';
 
-class ExpenseScreen extends StatelessWidget {
-  ExpenseScreen({super.key});
+class ExpenseScreen extends StatefulWidget {
+  const ExpenseScreen({super.key});
 
   @override
+  State<ExpenseScreen> createState() => _ExpenseScreenState();
+}
+
+class _ExpenseScreenState extends State<ExpenseScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Expense screen'),
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color.fromRGBO(254, 254, 248, 0.8),
+              Colors.white,
+            ],
+          ),
+        ),
+        child: const Column(
+          children: [
+            ExpenseCard(),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<double> getTotalExpenses() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return 24537.32;
   }
 }
