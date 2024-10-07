90127735b78bde962f0cc12e9910ea9951a5a29d
Dusan Cojic <dusancojic@gmail.com>
2024-10-07 17:24:31 +0200

    feat: button layout

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -7,18 +7,19 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(245, 245, 247, 1.0),
+      backgroundColor: const Color.fromRGBO(233, 234, 236, 0.2),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Padding(
-            padding: EdgeInsets.only(top: 20.0, right: 150.0, bottom: 20.0),
+            padding: EdgeInsets.only(top: 10.0, left: 40.0),
             child: SizedBox(
               child: Text(
                 "Settings",
                 style: TextStyle(
-                  fontSize: 38.0,
-                  color: Colors.black54,
+                  fontSize: 24,
                   fontWeight: FontWeight.bold,
+                  color: Colors.black54,
                 ),
               ),
             ),
@@ -27,7 +28,7 @@ class SettingsScreen extends StatelessWidget {
             child: Column(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(bottom: 20.0),
+                  padding: const EdgeInsets.only(top: 20.0),
                   child: ElevatedButton(
                     onPressed: () {
                       showModalBottomSheet(
@@ -45,10 +46,80 @@ class SettingsScreen extends StatelessWidget {
                     style: ElevatedButton.styleFrom(
                       elevation: 0,
                       padding: const EdgeInsets.symmetric(
-                          horizontal: 120.0, vertical: 15.0),
+                        horizontal: 120.0,
+                        vertical: 15.0,
+                      ),
+                      backgroundColor: Colors.white,
                     ),
                     child: const Text(
                       "Edit categories",
+                      style: TextStyle(
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15.0, left: 25.0),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 90.0,
+                              vertical: 15.0,
+                            ),
+                            backgroundColor: Colors.white,
+                          ),
+                          child: const Text(
+                            "Edit currency",
+                            style: TextStyle(
+                              color: Colors.black87,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        child: Container(
+                          width: 65.0,
+                          height: 50.0,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              "USD",
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 130.0,
+                        vertical: 15.0,
+                      ),
+                      backgroundColor: Colors.white,
+                    ),
+                    child: const Text(
+                      "Export data",
                       style: TextStyle(
                         color: Colors.black87,
                       ),
